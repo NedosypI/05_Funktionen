6aa258734fc396fe7e4ca8fe7c8e5b1ca960a601
NedosypI <irinanedosyp@gmx.de>
2022-07-11 15:05:29 +0200

    Funktionen 02a

--- script_01.js
+++ script_01.js
@@ -5,12 +5,27 @@
 // 1. Kapselung von Codeblöcken
 
 //Funktionsaufruf (call)
-test();
+// test();
 
 // Funktionsrumpf (body) | callee
 function test()
 {
-console.log("Hallo Robi");
+console.log("Hallo Robert");
 
 }
+
+/***** Funktionen 02a *****/
+//2a. Parametrisierung + Datenübergabe von INNEN
+
+ausgabeNamen();
+
+
+function ausgabeNamen() {
+    let firstName = "Tom"; //what happens in Vegas,...
+    console.log("Hallo " + firstName + "!");
+}
+
+// console.log(firstName); // Fehler: Scope!
+
+
 
