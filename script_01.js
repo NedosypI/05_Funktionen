0b11c2e54fc4b0c9ede45fcb0ddef89d866e7224
NedosypI <irinanedosyp@gmx.de>
2022-07-11 14:41:49 +0200

    Funktionen 01

--- script_01.js
+++ script_01.js
@@ -2,3 +2,15 @@
 
 /***** Funktionen 01 *****/
 
+// 1. Kapselung von Codeblöcken
+
+//Funktionsaufruf (call)
+test();
+
+// Funktionsrumpf (body) | callee
+function test()
+{
+console.log("Hallo Robi");
+
+}
+
